24e2480b726452336a0787c11cee3559837d828f
曾伟楠 <449041992@qq.com>
2018-05-04 16:17:37 +0800

    测试依赖注入普通参数

--- structure/inversion/B.php
+++ structure/inversion/B.php
@@ -13,15 +13,17 @@ namespace structure\inversion;
 class B
 {
     private $a;
+    private $people;
 
-    public function __construct(A $a)
+    public function __construct(A $a, $people = 'my')
     {
         $this->a = $a;
+        $this->people = $people;
     }
 
     public function test()
     {
         $this->a->test();
-        echo __METHOD__ . '|';
+        echo __METHOD__ . '|' . "普通参数:{$this->people}|";
     }
 }
